76eac2836f7fd9a9c6af1994d65a547c2b524afe
clap-0 <clap-0@naver.com>
2021-09-29 14:29:03 +0900

    feat: Add clock

---
+++ js/clock.js
@@ -0,0 +1,14 @@
+const clock = document.querySelector("h2#clock");
+
+// 현재 시각을 불러와서 출력한다
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+// setInterval()은 처음 1000ms가 지난 후 실행되므로 먼저 한번 실행한다
+getClock();
+setInterval(getClock, 1000);
